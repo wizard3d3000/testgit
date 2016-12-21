f4c9cf6fe036e192cfe6949b4f50a4113d081d12
igor <wizard3d3000@yandex.ru>
2016-12-21 18:48:29 +0300

    add some diferent code

--- index.php
+++ index.php
@@ -10,13 +10,16 @@ class testClass
     public function index()
     {
         echo 'test';
-<<<<<<< HEAD
+
         $a = 10;
-        for ($i = 0; $i<$a; $i++){
+        for ($i = 0; $i < $a; $i++) {
             echo 'Magento';
+            
+            
         }
-=======
-        echo 'test2';
->>>>>>> 661088fc77042f73eb07f07bb53eec23d901664b
+        
+        echo 'magento';
     }
-}
+}    
+ 
+        
